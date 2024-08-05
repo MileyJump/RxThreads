1cd95b866d4f3c0beecaf9e4e8543b97b1d7b22d
choiminkyung <choialsrud2002@gmail.com>
2024-08-05 23:17:20 +0900

    .

---
+++ RxThreads/PasswordViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  PasswordViewModel.swift
+//  RxThreads
+//
+//  Created by 최민경 on 8/5/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class PasswordViewModel {
+    
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}
